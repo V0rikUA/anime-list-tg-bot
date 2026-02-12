678c6ca146fd57dfa9fa0f0167f7b27c76586378
Dmitriy Kirov <kirovdmitriy@gmail.com>
2026-02-12 05:27:55 +0200

    fix

---
+++ bot-service/migrations/202602120001_add_user_watch_progress.cjs
@@ -0,0 +1,9 @@
+/** @param {import('knex').Knex} knex */
+exports.up = async function up(knex) {
+  // Compatibility placeholder: migration is owned by list-service.
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function down(knex) {
+  // No-op.
+};
